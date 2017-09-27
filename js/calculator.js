bf50e44cfb060c37f05d233c7119de1a64081482
cazyw <kazeisc@gmail.com>
2017-09-27 21:34:37 +1000

    Fix '.' and delete feature

--- js/calculator.js
+++ js/calculator.js
@@ -3,10 +3,11 @@ let commands = [];
 let calc = "";
 let temp_n = 0;
 let sym = false;
+let frac = 0;
 
-const printScreen = () => {
-    $("#screen-big").text(total);
-    $("#screen-little").text(commands.join(""));
+const printScreen = (big, small) => {
+    $("#screen-big").text(big);
+    $("#screen-little").text(small);
 }
 
 $(document).ready(function() {
@@ -15,24 +16,31 @@ $(document).ready(function() {
         let entry = $(e.target).text(); 
         console.log(entry, "temp_n", temp_n, "total", total, calc, commands);
         if(entry.match(/^[0-9]+$/)){
-            temp_n *= 10;
-            temp_n += parseInt(entry);
-
+            if (frac > 0){
+                console.log("power", entry * Math.pow(10, -frac));
+                console.log("temp in nums 1", temp_n);
+                temp_n += parseFloat(entry) * Math.pow(10, -frac);
+                console.log("temp in nums 2", temp_n);
+                frac++;
+            }
+            else {
+                temp_n *= 10;
+                temp_n += parseFloat(entry);
+            }
+            console.log("temp_n", temp_n);
             // total = temp_n;
             $("#screen-big").text(temp_n);
             if(calc === "") {
                 $("#screen-little").text("");
             }
-            commands.push(entry);
+            // commands.push(entry);
             sym = false;
         }
         else if (entry === ".") {
             console.log(entry);
-            total = 0;
-            commands = [];
-            calc = "";
-            temp_n = 0;
-            printScreen();
+            temp_n = parseFloat(temp_n.toFixed(1));
+            console.log("temp_n", temp_n);
+            frac = 1;
             sym = false;
         }
         else if (entry === "C") {
@@ -41,15 +49,24 @@ $(document).ready(function() {
             commands = [];
             calc = "";
             temp_n = 0;
-            printScreen();
+            printScreen(total, commands.join(" "));
             sym = false;
+            frac = 0;
         }
         else if (entry === "DEL") {
             console.log(entry);
-            temp_n = Math.floor(temp_n / 10);
-            commands.pop();
-            $("#screen-big").text(temp_n);
+            if(temp_n !== 0){
+                let mod = temp_n.toString();
+                mod = mod.slice(0,mod.length-1);
+                temp_n = parseFloat(mod);
+            }
+            if(sym){
+                commands.pop();
+            }
+            printScreen(temp_n, commands.join(" "));
+            // $("#screen-big").text(temp_n);
             sym = false;
+            frac = 0;
         }
             // else if (entry === "=") {
             //     console.log(entry);
@@ -61,66 +78,86 @@ $(document).ready(function() {
             //     clear = true;
             // }
         else {
+            frac = 0;
             if (sym){
                 console.log("double");
                 calc = entry;
                 commands.pop();
                 commands.push(entry);
-                printScreen();
+                printScreen(total, commands.join(" "));
             }
             else if (calc === "") {
                 total = temp_n;
-                commands.push(entry);
+                commands.push(temp_n);
+                if (entry !== "="){
+                    commands.push(entry);
+                }
                 $("#screen-big").text(total);
-                $("#screen-little").text(commands.join(""));
+                $("#screen-little").text(commands.join(" "));
                 calc = entry;
                 if ($(e.target).attr('id') === "div") {
                     console.log("divide!");
                     calc = "/";
                 }
                 temp_n = 0;
-                // total += parseInt(temp_n);
+                // total += parseFloat(temp_n);
                 
             }
             else if (calc === "+") {
                 calc = "+";
-                commands.push(entry);
-                total += parseInt(temp_n);
-                printScreen();
+                commands.push(temp_n);
+                if (entry !== "="){
+                    commands.push(entry);
+                }
+                total += parseFloat(temp_n);
+                printScreen(total, commands.join(" "));
                 temp_n = 0;
                 calc = entry;
                 
             }
             else if (calc === "-") {
                 calc = "-";
-                commands.push(entry);
-                total -= parseInt(temp_n);
-                printScreen();
+                commands.push(temp_n);
+                if (entry !== "="){
+                    commands.push(entry);
+                }
+                total -= parseFloat(temp_n);
+                printScreen(total, commands.join(" "));
                 temp_n = 0;
                 calc = entry;
             }
             else if (calc === "/") {
                 calc = "/";
-                commands.push(entry);
-                total /= parseInt(temp_n);
-                printScreen();
+                commands.push(temp_n);
+                if (entry !== "="){
+                    commands.push(entry);
+                }
+                total /= parseFloat(temp_n);
+                printScreen(total, commands.join(" "));
                 temp_n = 0;
                 calc = entry;
             }
             else if (calc === "x") {
                 calc = "x";
-                commands.push(entry);
-                total *= parseInt(temp_n);
-                printScreen();
+                commands.push(temp_n);
+                if (entry !== "="){
+                    commands.push(entry);
+                }
+                total *= parseFloat(temp_n);
+                printScreen(total, commands.join(" "));
                 temp_n = 0;
                 calc = entry;
             }
             if (entry === "=") {
-                commands.pop();
-                printScreen();
+                // console.log("eq", temp_n);
+                if (!(commands[commands.length-1] === "=")){
+                    commands.push("=");
+                }
+                commands.push(total);
+                printScreen(total, commands.join(" "));
                 calc = "";
                 // commands.push(entry);
-                // total -= parseInt(temp_n);
+                // total -= parseFloat(temp_n);
                 commands = [];
                 total = 0;
                 temp_n = 0;
@@ -130,24 +167,3 @@ $(document).ready(function() {
         
     });
 });
-
-// if (calc === "+") {
-//     total += parseInt(entry);
-//     commands.push(entry);
-// }
-// else if (calc === "-") {
-//     total -= parseInt(entry);
-//     commands.push(entry);
-// }
-// else if (calc === "%") {
-//     total /= parseInt(entry);
-//     commands.push(entry);
-// }
-// else if (calc === "x") {
-//     total /= parseInt(entry);
-//     commands.push(entry);
-// }
-// else if (calc === "=") {
-//     total = entry;
-//     commands = [];
-// }  
