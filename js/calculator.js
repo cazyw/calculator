fd06cb522fd0c8111375ffff245125d664efb3d4
cazyw <kazeisc@gmail.com>
2017-09-27 23:00:56 +1000

    Refactor code into separate functions

--- js/calculator.js
+++ js/calculator.js
@@ -1,13 +1,24 @@
-let total = 0;
-let commands = [];
-let calc = "";
-let temp_n = 0;
-let sym = false;
-let frac = 0;
+let total = 0; // current total 
+let commands = []; // commands entered
+let calc = ""; // operator
+let temp_n = 0; // current number
+let sym = false; // operator vs digit entered to catch if operator entered multiple times
+let frac = 0; // number is a float "." entered
 
 const printScreen = (big, small) => {
-    $("#screen-big").text(big);
-    $("#screen-little").text(small);
+    if(small.length > 35 || big.toString().length > 14){
+        commands = ["Max digits reached"];
+        $("#screen-little").text("Max digits reached");
+        clearVal();
+        setTimeout( function(){ 
+            $("#screen-big").text("");
+            $("#screen-little").text("");
+          }, 2000 );
+    }
+    else {
+        $("#screen-big").text(big);
+        $("#screen-little").text(small);
+    }
 }
 
 const clearVal = () => {
@@ -17,7 +28,111 @@ const clearVal = () => {
     temp_n = 0;
 }
 
+const updateVal = (val) => {
+    commands.push(temp_n);
+    if(val !== "="){
+        commands.push(val);
+    }
+    printScreen(total, commands.join(" "));
+    temp_n = 0;
+    calc = val;
+}
 
+const updateFlags = (f, s) => {
+    frac = f;
+    sym = s;
+}
+
+const operatorEntered = (entry) => {
+    // to catch if the previous entry was an operator
+    if (sym){
+        calc = entry;
+        commands.pop();
+        commands.push(entry);
+        printScreen(total, commands.join(" "));
+    }
+    else if (calc === "DEL") {
+        calc = entry;
+        commands.push(entry);
+        printScreen(total, commands.join(" "));
+    }
+    else if (calc === "") {
+        total = temp_n;
+        printScreen(total, commands.join(" "));
+        updateVal(entry);
+    }
+    else if (calc === "+") {
+        total += parseFloat(temp_n);
+        updateVal(entry);
+    }
+    else if (calc === "-") {
+        total -= parseFloat(temp_n);
+        updateVal(entry);
+    }
+    else if (calc === "/") {
+        total /= parseFloat(temp_n);
+        updateVal(entry);
+    }
+    else if (calc === "x") {
+        total *= parseFloat(temp_n);
+        updateVal(entry);
+    }
+
+    if (entry === "=") {
+        if (!(commands[commands.length-1] === "=")){
+            commands.push("=");
+        }
+        commands.push(total);
+        printScreen(total, commands.join(" "));
+        clearVal();
+    }
+    
+    updateFlags(0, true);
+}
+
+const calculate = (entry) => {
+    if(entry.match(/^[0-9]+$/)){
+        if (frac > 0){
+            temp_n += parseFloat(entry) * Math.pow(10, -frac);
+            frac++;
+        }
+        else {
+            temp_n *= 10;
+            temp_n += parseFloat(entry);
+        }
+        printScreen(temp_n, commands.join(" "));
+        sym = false;
+    }
+    else if (entry === ".") {
+        temp_n = parseFloat(temp_n.toFixed(1));
+        updateFlags(1, false);
+    }
+    else if (entry === "C") {
+        clearVal();
+        printScreen(total, commands.join(" "));
+        updateFlags(0, false);
+    }
+    else if (entry === "DEL") {
+        if(temp_n !== 0){
+            if (temp_n.toString().length === 1) {
+                temp_n = 0;
+            }
+            else {
+                temp_n = parseFloat(temp_n.toString().slice(0,temp_n.toString().length-1));
+            }
+            printScreen(temp_n, commands.join(" "));
+        }
+        if(sym){
+            commands.pop();
+            printScreen(total, commands.join(" "));
+            calc = "DEL";
+        }
+        updateFlags(0, false);
+    }
+    else {
+        operatorEntered(entry);
+    }
+} 
 
 $(document).ready(function() {
     $(".but").on("click", e => {
@@ -25,125 +140,7 @@ $(document).ready(function() {
         if ($(e.target).attr('id') === "div") {
             entry = "/";
         }
-        if(entry.match(/^[0-9]+$/)){
-            if (frac > 0){
-                temp_n += parseFloat(entry) * Math.pow(10, -frac);
-                frac++;
-            }
-            else {
-                temp_n *= 10;
-                temp_n += parseFloat(entry);
-            }
-            printScreen(temp_n, commands.join(" "));
-            sym = false;
-        }
-        else if (entry === ".") {
-            temp_n = parseFloat(temp_n.toFixed(1));
-            frac = 1;
-            sym = false;
-        }
-        else if (entry === "C") {
-            clearVal();
-            printScreen(total, commands.join(" "));
-            sym = false;
-            frac = 0;
-        }
-        else if (entry === "DEL") {
-            if(temp_n !== 0){
-                if (temp_n < 10) {
-                    temp_n = 0;
-                }
-                else {
-                    let mod = temp_n.toString();
-                    mod = mod.slice(0,mod.length-1);
-                    temp_n = parseFloat(mod);
-                }
-                
-                printScreen(temp_n, commands.join(" "));
-            }
-            if(sym){
-                commands.pop();
-                printScreen(total, commands.join(" "));
-                calc = "DEL";
-            }
-            sym = false;
-            frac = 0;
-        }
-        else {
-            frac = 0;
-            if (sym){
-                calc = entry;
-                commands.pop();
-                commands.push(entry);
-                printScreen(total, commands.join(" "));
-            }
-            else if (calc === "DEL") {
-                calc = entry;
-                commands.push(entry);
-                printScreen(total, commands.join(" "));
-            }
-            else if (calc === "") {
-                total = temp_n;
-                commands.push(temp_n);
-                if (entry !== "="){
-                    commands.push(entry);
-                }
-                printScreen(total, commands.join(" "));
-                calc = entry;
-                temp_n = 0;
-                
-            }
-            else if (calc === "+") {
-                commands.push(temp_n);
-                if (entry !== "="){
-                    commands.push(entry);
-                }
-                total += parseFloat(temp_n);
-                printScreen(total, commands.join(" "));
-                temp_n = 0;
-                calc = entry;
-                
-            }
-            else if (calc === "-") {
-                commands.push(temp_n);
-                if (entry !== "="){
-                    commands.push(entry);
-                }
-                total -= parseFloat(temp_n);
-                printScreen(total, commands.join(" "));
-                temp_n = 0;
-                calc = entry;
-            }
-            else if (calc === "/") {
-                commands.push(temp_n);
-                if (entry !== "="){
-                    commands.push(entry);
-                }
-                total /= parseFloat(temp_n);
-                printScreen(total, commands.join(" "));
-                temp_n = 0;
-                calc = entry;
-            }
-            else if (calc === "x") {
-                commands.push(temp_n);
-                if (entry !== "="){
-                    commands.push(entry);
-                }
-                total *= parseFloat(temp_n);
-                printScreen(total, commands.join(" "));
-                temp_n = 0;
-                calc = entry;
-            }
-            if (entry === "=") {
-                if (!(commands[commands.length-1] === "=")){
-                    commands.push("=");
-                }
-                commands.push(total);
-                printScreen(total, commands.join(" "));
-                clearVal();
-            }
-            sym = true;
-        }
+        calculate(entry);
         
     });
 });
